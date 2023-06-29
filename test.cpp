eeac36d88713ddf1ecfba8135efa9a25295ab7ad
Szabolcs Horvát <szhorvat@gmail.com>
2023-06-29 14:15:50 +0200

    tests: expand test to serve as example

--- test.cpp
+++ test.cpp
@@ -6,13 +6,30 @@
 using namespace std;
 
 int main() {
+
+    // igRealVec is a RAII wrapper to igraph_vector_t.
+    // When it goes out of scope, it is properly destroyed.
+    // igRealVec is a convenience name for igVec<igraph_real_t>.
     igRealVec v;
 
+    // initial size is zero
     cout << v.size() << endl;
 
-    igraph_vector_resize(v, 10);
-
+    // igVec<XXX> is convertible to the corresponding igraph_vector_XXX_t * pointer.
+    igCheck(igraph_vector_resize(v, 10));
     cout << v.size() << endl;
+
+    // igVec<> has STL-compatible convenience member functions for common operations.
+    v.resize(5);
+    cout << v.size() << endl;
+
+    // The indexing operator is available.
+    v[3] = 1; v[4] = 2;
+
+    // The STL comatibility provides support for range-based for loops.
+    for (auto el : v)
+        cout << el << ' ';
+    cout << endl;
 
     return 0;
 }
