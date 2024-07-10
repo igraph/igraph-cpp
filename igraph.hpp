2279351055833e9b675a92dd394a574f855fbb31
Szabolcs Horvát <szhorvat@gmail.com>
2024-07-10 10:44:56 +0000

    feat: convenience functions for oft-queried basic graph properties

--- igraph.hpp
+++ igraph.hpp
@@ -85,6 +85,68 @@ public:
     bool is_directed() const { return igraph_is_directed(&graph); }
     igraph_integer_t vcount() const { return igraph_vcount(&graph); }
     igraph_integer_t ecount() const { return igraph_ecount(&graph); }
+
+    // Convenience access to basic properties (mostly those that are cached).
+
+    bool is_connected(igraph_connectedness_t mode = IGRAPH_WEAK) const {
+        igraph_bool_t res;
+        igCheck(igraph_is_connected(&graph, &res, mode));
+        return res;
+    }
+
+    bool is_simple() const {
+        igraph_bool_t res;
+        igCheck(igraph_is_simple(&graph, &res));
+        return res;
+    }
+
+    bool has_loop() const {
+        igraph_bool_t res;
+        igCheck(igraph_has_loop(&graph, &res));
+        return res;
+    }
+
+    bool has_multi() const {
+        igraph_bool_t res;
+        igCheck(igraph_has_multiple(&graph, &res));
+        return res;
+    }
+
+    bool has_mutual(bool loops = false) const {
+        igraph_bool_t res;
+        igCheck(igraph_has_mutual(&graph, &res, loops));
+        return res;
+    }
+
+    bool is_tree(igraph_neimode_t mode = IGRAPH_ALL) const {
+        igraph_bool_t res;
+        igCheck(igraph_is_tree(&graph, &res, nullptr, mode));
+        return res;
+    }
+
+    bool is_forest(igraph_neimode_t mode = IGRAPH_ALL) const {
+        igraph_bool_t res;
+        igCheck(igraph_is_forest(&graph, &res, nullptr, mode));
+        return res;
+    }
+
+    bool is_dag() const {
+        igraph_bool_t res;
+        igCheck(igraph_is_dag(&graph, &res));
+        return res;
+    }
+
+    bool is_acyclic() const {
+        igraph_bool_t res;
+        igCheck(igraph_is_acyclic(&graph, &res));
+        return res;
+    }
+
+    igraph_real_t mean_degree(bool loops = true) const {
+        igraph_real_t res;
+        igCheck(igraph_mean_degree(&graph, &res, loops));
+        return res;
+    }
 };
 
 } // namespace ig
--- test.cpp
+++ test.cpp
@@ -104,5 +104,10 @@ int main() {
         std::cout << "Degrees: " << deg << std::endl;
     }
 
+    std::cout << "Mean degree: " << g.mean_degree() << std::endl;
+    std::cout << "Tree? " << (g.is_tree() ? "Yes" : "No") << std::endl;
+    std::cout << "Connected? " << (g.is_connected() ? "Yes" : "No") << std::endl;
+    std::cout << "Has loops? " << (g.has_loop() ? "Yes" : "No") << std::endl;
+
     return 0;
 }
