60aa722cea8aa642b6ae9ba5a1b694d43765bb3e
Szabolcs Horvát <szhorvat@gmail.com>
2024-07-10 09:08:32 +0000

    tests: expand tests/examples

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include "igraph.hpp"
 
 #include <iostream>
+#include <numeric>
 
 // Everything lives in the ig:: namespace.
 // We do not use igraph:: to avoid conflict with the igraphpp project.
@@ -28,7 +29,19 @@ int main() {
     // The indexing operator is available.
     v[3] = 1; v[4] = 2;
 
-    // The STL comatibility provides support for range-based for loops.
+    // The STL compatibility provides support for range-based for loops.
+    for (auto el : v)
+        std::cout << el << ' ';
+    std::cout << std::endl;
+
+    // Fill the vector with an increasing range of elements and print it.
+    std::iota(v.begin(), v.end(), 3);
+    for (auto el : v)
+        std::cout << el << ' ';
+    std::cout << std::endl;
+
+    // Erase the 2nd and 3rd elements.
+    v.erase(v.begin() + 1, v.begin() + 3);
     for (auto el : v)
         std::cout << el << ' ';
     std::cout << std::endl;
